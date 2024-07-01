bee4297d1e4cc5568f3effa3f4058f96c68dab3f
Trym Jakobsen <trjak5231@uib.no>
2024-07-01 21:11:02 +0200

    Format fixed using prettier

--- src/app/my-blog/page.tsx
+++ src/app/my-blog/page.tsx
@@ -6,7 +6,7 @@ export default function MyBlog() {
   return (
     <div className="flex flex-col min-h-screen bg-gradient-to-b from-indigo-500 via-purple-500 to-pink-500">
       <Navigation />
-      <div className="max-w-screen-sm mx-auto text-lg flex flex-col gap-6 py-24">
+      <div className="max-w-screen-lg mx-auto text-lg flex flex-col gap-6 py-24">
         <h2 className="text-2xl text-gray-100 mb-4">My blog:</h2>
         <BlogPosts />
       </div>
