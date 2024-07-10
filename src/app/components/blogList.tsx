ecbe037aa56f2385e7ae7b338659c983099bc317
Trym Jakobsen <trjak5231@uib.no>
2024-07-10 19:37:58 +0200

    Now uses the API to update the amount of likes through a button on each blog post. Also use postId as key for each blog post.

--- src/app/components/blogList.tsx
+++ src/app/components/blogList.tsx
@@ -1,12 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from 'axios';
 import { posts } from "@/utils/blogPosts";
 const BlogPosts: React.FC = () => {
+    const[likes, steLikes] = useState<{ [key: number]: number }>({});
+
+    const handleLike = async (postId: number) => {
+        try {
+            const response = await axios.post('/api/like', {postId});
+            const updatedLikes = response.data.likes;
+            setLikes((prevLikes) => ({...prevLikes, [postId]: updatedLikes}));
+        } catch (error) {
+            console.error('Failed to like post', error);
+        }
+    };
+
   return (
     <div className="bg-gradient-to-b from-indigo-500 via-purple-500 to-pink-500 p-8 rounded-lg shadow-lg">
       <ul className="space-y-4">
         {posts.map((post) => (
           <li
-            key={post.title}
+            key={post.postId}
             className="bg-gradient-to-b from-emerald-500 from-10% via-sky-500 via-30% to-indigo-500 to-90% p-8 rounded-lg shadow-lg"
           >
             <h3 className="text-white text-xl">{post.title}</h3>
@@ -21,6 +34,11 @@ const BlogPosts: React.FC = () => {
                 <br></br>
               </div>
             ))}
+              <button
+                  onClick={() => handleLike(post.postId)} className="mt-2 p-2 bg-blue-500 text-white rounded">
+                  Like
+              </button>
+              <p className="text-gray-200">Likes: {likes[post.postId] || 0}</p>
           </li>
         ))}
       </ul>
